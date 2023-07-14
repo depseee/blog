0479c5425a3026de451c87f096192fd4ee2c1549
depseee <raysikh1990@mail.ru>
2023-07-14 14:01:57 +0300

    third commit

--- news/urls.py
+++ news/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.news_home, name='news_home'),
+    path('create', views.NewsCreateView.as_view(), name='create'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='detail'),
+    path('<int:pk>/update', views.NewsUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete', views.NewsDeleteView.as_view(), name='delete'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Articles
+from .forms import ArticlesForm
+from django.views.generic import DetailView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView
 
 
 # Create your views here.
@@ -7,3 +10,29 @@ from .models import Articles
 def news_home(request):
     news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
+
+
+class NewsCreateView(CreateView):
+    model = Articles
+    form_class = ArticlesForm
+    template_name = 'news/create.html'
+    success_url = '/news'
+
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/detail.html'
+    context_object_name = 'article'
+
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    form_class = ArticlesForm
+    template_name = 'news/update.html'
+    success_url = '/news'
+
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    template_name = 'news/delete.html'
+    success_url = '/news'
